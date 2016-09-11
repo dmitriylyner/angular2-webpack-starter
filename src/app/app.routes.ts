8b413688446676cb50bb77b62727e5a9629b3723
dima <dmitriylyner@gmail.com>
2016-09-11 10:27:15 -0600

    adding client shell

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { Home } from './home';
 import { About } from './about';
+import { Login } from './login';
 import { NoContent } from './no-content';
 
 import { DataResolver } from './app.resolver';
@@ -10,6 +11,7 @@ export const ROUTES: Routes = [
   { path: '',      component: Home },
   { path: 'home',  component: Home },
   { path: 'about', component: About },
+  { path: 'login', component: Login },
   {
     path: 'detail', loadChildren: () => System.import('./+detail')
   },
